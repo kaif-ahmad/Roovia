61b984968324ed1d7a59c2cab7e8ccaa187e6e39
Kaif Ahmad <kaifthehero133@gmail.com>
2025-06-21 23:40:46 +0530

    init DB

---
+++ init/index.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js"); //object of array ie data { data[{},{}...] }
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+main().then(res => { console.log("CONNECTED TO DB") }).catch(err => { console.log(err) });
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Data was Initialised");
+}
+
+initDB();
