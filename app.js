564f7907f619a9e3582325b6032c92b4c89e1510
Kaif Ahmad <kaifthehero133@gmail.com>
2025-06-22 16:27:26 +0530

    basic setup done

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride=require("method-override");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 //============================================================================
 //  Mongoose Connection
@@ -11,22 +18,62 @@ main().then(res => { console.log("CONNECTED TO DB") }).catch(err => { console.lo
 async function main() {
     await mongoose.connect(MONGO_URL);
 }
+//============================================================================
+//  Delete Route
+
+app.delete("/listings/:id", async (req, res) => {
+    let {id}=req.params;
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
 
 //============================================================================
-// Testing Route
+//  Edit Route
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "By the Beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India"
-    });
-    await sampleListing.save();
-    console.log("Sample was Saved");
-    res.send("Success");
+//  Details
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
 })
+//  Editin on DB using POST
+app.put("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//============================================================================
+//  Add New Route
+
+//  Details
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+})
+//  Adding on DB using POST
+app.post("/listings",async (req,res)=>{
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+
+//============================================================================
+//  Show Route
+
+app.get("/listings/:id", async (req, res) => {
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+
+//============================================================================
+//  Index Route
+
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+});
 
 //============================================================================
 //  Root Route
@@ -34,6 +81,22 @@ app.get("/testListing", async (req, res) => {
 app.get("/", (req, res) => {
     res.send("ROOT Working");
 })
+
+//============================================================================
+// Testing Route
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "By the Beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India"
+//     });
+//     await sampleListing.save();
+//     console.log("Sample was Saved");
+//     res.send("Success");
+// })
 
 //============================================================================
 
