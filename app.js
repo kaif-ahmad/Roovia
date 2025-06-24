cd986bc8fbb86db702e59260848aa5ef85b36120
Kaif Ahmad <kaifthehero133@gmail.com>
2025-06-24 17:27:01 +0530

    Phase-One

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema}=require("./schema.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -15,10 +16,20 @@ app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"public")));
 
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,error);
+    }else{
+        next();
+    }
+}
+
 //============================================================================
 //  Mongoose Connection
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+const MONGO_URL = 'mongodb://127.0.0.1:27017/roovia';
 main().then(res => { console.log("CONNECTED TO DB") }).catch(err => { console.log(err) });
 async function main() {
     await mongoose.connect(MONGO_URL);
@@ -38,10 +49,7 @@ app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs");
 })
 //  Adding on DB using POST
-app.post("/listings",wrapAsync(async (req,res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400,"Send Valid Data!");
-    }
+app.post("/listings",validateListing,wrapAsync(async (req,res)=>{
     const newListing=new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -66,7 +74,7 @@ app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
     res.render("listings/edit.ejs",{listing});
 }))
 //  Editin on DB using POST
-app.put("/listings/:id",wrapAsync(async (req,res)=>{
+app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>{
     if(!req.body.listing){
         throw new ExpressError(400,"Send Valid Data!");
     }
@@ -84,6 +92,7 @@ app.delete("/listings/:id",wrapAsync(async (req, res) => {
     console.log(deletedListing);
     res.redirect("/listings");
 }));
+
 //============================================================================
 //  ERRORS
 
@@ -93,7 +102,8 @@ app.delete("/listings/:id",wrapAsync(async (req, res) => {
 
 app.use((err,req,res,next)=>{
     let {statusCode=500,message="Something Went Wrong!"}=err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs",{err});
+    //res.status(statusCode).send(message);
 })
 
 //============================================================================
--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js"); //object of array ie data { data[{},{}...] }
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+const MONGO_URL = 'mongodb://127.0.0.1:27017/roovia';
 main().then(res => { console.log("CONNECTED TO DB") }).catch(err => { console.log(err) });
 async function main() {
     await mongoose.connect(MONGO_URL);
